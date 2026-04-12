a7451024243ccb405939956668695f785ba09077
AarySlayz <aaryusagar12@gmail.com>
2026-04-12 15:54:13 -0400

    setup

---
+++ main/include/helpers.h
@@ -0,0 +1,24 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <stdio.h>
+
+// PIN DEFINITIONS
+
+#define C_PIN 15
+#define C_SHARP_PIN 2
+#define D_PIN 0
+#define D_SHARP_PIN 4
+#define E_PIN 16
+#define F_PIN 17
+#define F_SHARP_PIN 5
+#define G_PIN 18
+#define G_SHARP_PIN 19
+#define A_PIN 21
+#define A_SHARP_PIN 13
+#define B_PIN 1
+
+#define DEBOUNCE_TIME 100000
+
+
+#endif // HELPERS_H
--- main/main.c
+++ main/main.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "driver/ledc.h"
+#include "driver/rmt_tx.h"  
+#include "driver/gpio.h"
+#include "helpers.h"
+
+// Input pin config
+gpio_config_t input_pin = {
+    .pin_bit_mask = (1ULL << C_PIN) |
+                    (1ULL << C_SHARP_PIN) |
+                    (1ULL << D_PIN) |
+                    (1ULL << D_SHARP_PIN) |
+                    (1ULL << E_PIN) |
+                    (1ULL << F_PIN) |
+                    (1ULL << F_SHARP_PIN) |
+                    (1ULL << G_PIN) |
+                    (1ULL << G_SHARP_PIN) |
+                    (1ULL << A_PIN) |
+                    (1ULL << A_SHARP_PIN) |
+                    (1ULL << B_PIN),
+    .mode = GPIO_MODE_INPUT,
+    .pull_up_en = GPIO_PULLUP_ENABLE,
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .intr_type = GPIO_INTR_NEGEDGE
+};
 
 void app_main(void)
 {
